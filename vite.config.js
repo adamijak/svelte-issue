e3b12f90f8637fce0102157eca3b54378fcef0f4
adamijak <jakub.adamik@greycorbel.com>
2023-09-27 20:50:45 +0200

    feat: bad configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build:{
+		lib:{
+			entry: './src/lib/index.js',
+			name: 'MyComponent',
+			fileName: 'my-component',
+			formats: ['es'],
+		},
+	},
 	plugins: [sveltekit()]
 });
