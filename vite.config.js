657e566ad24bd2a8c7bb45646dd95597036ff0c0
adamijak <jakub.adamik@greycorbel.com>
2024-03-26 15:12:36 +0100

    feat: build

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build:{
-		lib:{
-			entry: './src/lib/index.js',
-			name: 'MyComponent',
-			fileName: 'my-component',
-			formats: ['es'],
-		},
-	},
 	plugins: [sveltekit()]
 });
